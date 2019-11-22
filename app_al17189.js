d4b913a8f028a9378788cc6388a271ff4f878648
Jorge Luis Carrillo Cuevas <al17189@cbtis051.edu.mx>
2019-11-22 15:14:35 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17189.js
+++ app_al17189.js
@@ -1,7 +1,15 @@
 var http = require('http');
 http.createServer(function (req, res) {
+
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola Mundo Node JS Repo GIT - Carrillo Cuevas Jorge Luis\n');
+    var objeto = {
+        mensaje : 'Hola Mundo Node JS Repo GIT - Carrillo Cuevas Jorge Luis\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+    
 }).listen(8081);
-console,log('Server running on port 8081');
+console.log('Server running on port 8081');
