cfda3dc4451525d93bee7558ea17b9934b24adcd
Diana Karina Gamboa Villalobos <al17197@cbtis051.edu.mx>
2019-11-26 10:19:32 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17197.js
+++ app_al17197.js
@@ -2,6 +2,11 @@ var http = require ('http');
 http.createServer(function(req, res) {
     res.writeHead(200,{'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola Mundo Node JS Repo GIT - Gamboa Villalobos Diana Karina\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Gamboa Villalobos Diana Karina\n'
+    };
+
+    var json = JSON.stringify(objeto);
+
 }).listen(8081);
 console.log('Server runnig on port 8081')
